1940b948e1fc081146aeffcaa6dc799cf76e2020
Diego Castellaro <die.cst@gmail.com>
2018-08-08 16:50:44 +0200

    create basic controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+categories = %w(chinese italian japanese french belgian)
+values = [0, 1, 2, 3, 4, 5]
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+5.times do
+  new_restaurant = Restaurant.new(
+    name: Faker::Superhero.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber,
+    category: categories.sample
+    )
+  new_restaurant.save
+  5.times do
+    review = Review.new(
+    content: Faker::Hipster.sentence,
+    rating: values.sample,
+    )
+    review.restaurant = new_restaurant
+    review.save
+  end
+end
+
+puts 'Done!'
